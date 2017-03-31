d914b7ad268028c0e6349b35438b19bfce38564f
Daniel Baah <dbaah@spatialdev.com>
2017-03-30 20:14:35 -0700

    use custom user details function for endpoint

--- api/custom/routes/vw_omk_user_details.js
+++ api/custom/routes/vw_omk_user_details.js
@@ -10,12 +10,12 @@ vwUserDetails.init = function (req, res, next) {
     var user = req.user;
 
     var db = pgb.getDatabase();
-    var sql = "SELECT * FROM ___select_one($1, $2, $3)";
+    var sql = "SELECT * FROM vw_omk_user_details($1);";
 
     // Request data from the database
-    db.manyOrNone(sql, [table, columns, recordId])
+    db.manyOrNone(sql, [recordId])
         .then(function (results) {
-            var returnObj = JSON.parse(results[0].___select_one);
+            var returnObj = JSON.parse(results[0]["vw_omk_user_details"]);
             res.status(200).json(returnObj);
         })
         .catch(function (error) {
